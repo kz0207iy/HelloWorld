5e6c366464c9c095467328a8ce6a2435ef6fd524
Manoj Panta <manojpanta@Manojs-MacBook-Pro.local>
2021-08-10 19:12:09 -0500

    Add advanced git commands

--- src/main/java/MustKnowGitCommands.java
+++ src/main/java/MustKnowGitCommands.java
@@ -1,8 +1,35 @@
+import org.omg.CORBA.RepositoryIdHelper;
+
 public class MustKnowGitCommands {
 
+
+//    PUSH OPERATION
 //    git status
 //    git add .  || git add <file-name>
 //    git commit -m "<-message->"
 //    git push origin <branch>
 //    git branch to check the current branch.
+
+//    PULL OPeration
+//    git pull origin <branch-name>
+//    git pull upstream <branch-name>
+
+
+//    advanced
+//    git remote -v -- google this // know more about what are other remotes
+//    how to add a different remote
+//    git stash
+//    git stash save <stash-name>
+//    git stash list
+//    git stash clear
+
+//    FORK in Github
+//    company_userame
+//        - Repositories
+//            -HelloWorld(upstream).git
+//
+//
+//    Manjilpanta
+//        -Repositories
+//            - HelloWorld(origin).git(comment: this is a fork of Helloworld above)
 }
